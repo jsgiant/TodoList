cfec50636ab7016aae8168ea1c7fd702f589c6cd
Anil Peddireddy <peddireddyanilchowdary@gmail.com>
2022-02-26 15:10:15 +0530

    Use todos provider and hook to render the todos

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,11 +1,18 @@
 import { MdDelete } from "react-icons/md";
-
+import { useTodos } from "../../hooks/useTodos";
+import { TodoType } from "../../types";
 import "./../../styles/todo.css";
-export const Todo = () => {
-  return (
-    <li className="todo-item">
-      <p className="todo-text"> Learn React </p>
-      <MdDelete className="delete-icon" />
-    </li>
-  );
+
+export const Todo = (props: { todo: TodoType }) => {
+	const { deleteTodo } = useTodos();
+	const { id, title } = props.todo;
+
+	const onDeleteTodo = () => deleteTodo(id);
+
+	return (
+		<li className='todo-item' key={id}>
+			<p className='todo-text'> {title} </p>
+			<MdDelete className='delete-icon' onClick={onDeleteTodo} />
+		</li>
+	);
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,12 +1,20 @@
+import { useTodos } from "../../hooks/useTodos";
 import { Todo } from "../Todo/Todo";
 import "./../../styles/todoList.css";
 export const TodoList = () => {
-  return (
-    <div className="todo-list-container">
-      <h1 className="todo-list-heading">Todo List</h1>
-      <ul className="todo-list">
-        <Todo />
-      </ul>
-    </div>
-  );
+	const { todos, isLoading, isFailure, error } = useTodos();
+	return (
+		<div className='todo-list-container'>
+			<h1 className='todo-list-heading'>Todo List</h1>
+			<ul className='todo-list'>
+				{isLoading ? (
+					<p>Loading....</p>
+				) : isFailure ? (
+					<p>{error}</p>
+				) : (
+					todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+				)}
+			</ul>
+		</div>
+	);
 };
